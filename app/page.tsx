751dad64201da0695b5cb7524dae08a4e4dce014
theskyworld <2456450687@qq.com>
2024-08-26 13:59:47 +0800

    feat: change page.tsx, install shadcn-ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
 
 export default function Home() {
-  return (<>
-    <p>添加/github-webhook 选择githubweb</p>
-    <p>执行命令的Transfer Set与上传文件的分开写</p>
-    <p>pm2 start npm --name cards-show -- start --</p>
-    <p>pm2 restart cards-show</p>
-  </>)
+  return null;
+  
 }
