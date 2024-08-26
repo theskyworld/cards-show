c9fd9885dcc77b9c7adedc128a67a573e93367d4
theskyworld <2456450687@qq.com>
2024-08-26 12:36:46 +0800

    feat: change page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,7 @@
 
 export default function Home() {
-  return <p>/github-webhook2 githubweb  </p>
+  return (<>
+    <p>添加/github-webhook 选择githubweb</p>
+    <p>执行命令的Transfer Set与上传文件的分开写</p>
+  </>)
 }
