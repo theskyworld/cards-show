526331e64706fa15307c79ebd15345fb46fd45fc
theskyworld <2456450687@qq.com>
2024-08-26 13:07:50 +0800

    feat: change page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,5 +3,6 @@ export default function Home() {
   return (<>
     <p>添加/github-webhook 选择githubweb</p>
     <p>执行命令的Transfer Set与上传文件的分开写</p>
+    <p>pm2 start npm --name cards-show -- start --</p>
   </>)
 }
