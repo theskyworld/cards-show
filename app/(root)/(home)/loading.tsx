a0c3937724a91b9b85141d056e48bf25a1460bf2
theskyworld <2456450687@qq.com>
2024-08-27 01:52:13 +0800

    feat: 完善首页的loading组件

--- app/(root)/(home)/loading.tsx
+++ app/(root)/(home)/loading.tsx
@@ -1,5 +1,25 @@
-export default function HomeLoading() {
+import { Skeleton } from "@/components/ui/skeleton"
+
+
+const HomePageLoading = () => {
   return (
-    <p>loading...</p>
+    <main className="flex-center paddings mx-auto w-full max-w-screen-2xl flex-col">
+      <section className="nav-padding w-full">
+        {/* 导航部分的skeleton */}
+        <Skeleton className="h-[274px] w-full rounded-lg bg-black-200/40" />
+      </section>
+
+      <section className="mt-6 flex w-full flex-col sm:mt-20">
+        {/* 海报部分的skeleton */}
+        <Skeleton className="h-10 w-56 bg-black-200/40" />
+        <div className="mt-12 flex w-full flex-wrap justify-center gap-16 sm:justify-start">
+          <Skeleton className="h-[440px] w-full sm:w-[356px] bg-black-200/40" />
+          <Skeleton className="h-[440px] w-full sm:w-[356px] bg-black-200/40" />
+          <Skeleton className="h-[440px] w-full sm:w-[356px] bg-black-200/40" />
+        </div>
+      </section>
+    </main>
   )
 }
+
+export default HomePageLoading
