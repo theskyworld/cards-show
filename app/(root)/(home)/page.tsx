4b218a557b614420dd81bcaa0523359f88b2a690
theskyworld <2456450687@qq.com>
2024-08-26 15:47:56 +0800

    feat: 添加首页中的海报

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -3,12 +3,16 @@ import { Button } from "@/components/ui/button";
 export default function HomePage() {
   return (
     <>
-      {/* 由于Footer的位置跟随Home页面高度的变化而变化，在这里放些内容，调整下Footer的位置，避免样式不好看 */}
-      <h1>home</h1>
-      <h1>home</h1>
-      <h1>home</h1>
-      <h1>home</h1>
-      <h1>home</h1>
+      <main className="flex-center paddings mx-auto w-full max-w-screen-2xl flex-col">
+        <section className="nav-padding w-full">
+          {/* 其中的bg-banner的效果通过backgroundImage配置选项进行指定 */}
+          <div className="flex-center relative min-h-[274px] w-full flex-col rounded-xl bg-banner bg-cover bg-center text-center">
+            <h1 className="sm:heading1 heading2 mb-6 text-center text-white">
+              JavaScript Mastery Resources
+            </h1>
+          </div>
+        </section>
+      </main>
 
     </>
   );
