79e86c4b7f9f7abbe9d64ea399666d1e63845828
theskyworld <2456450687@qq.com>
2024-08-26 14:39:13 +0800

    feat: 新建Navbar和Footer组件并使用

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,9 +1,12 @@
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
+
 export default function HomeLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <p>NavBar</p>
+      <Navbar />
       {children}
-      <p>Footer</p>
+      <Footer />
     </>
   )
 }
---
+++ components/Navbar.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+import Image from "next/image";
+
+export default function Navbar() {
+  return (
+    <nav className="flex-center fixed top-0 z-50 w-full border-b-2 border-black-200 py-7 text-white">
+      <div className="flex-between mx-auto w-full max-w-screen-2xl px-6 xs:px-8 sm:px-16">
+        <Link href="/">
+          <Image
+            src=""
+            alt="logo"
+            width={55}
+            height={40}
+          />
+        </Link>
+      </div>
+
+    </nav>
+  )
+}
