f846a7c214fbafefad67b21d0117cad4d823cf4d
theskyworld <2456450687@qq.com>
2024-08-26 15:28:28 +0800

    feat: 修改Footer并使用

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,5 +1,15 @@
 import { Button } from "@/components/ui/button";
 
 export default function HomePage() {
-  return null;
+  return (
+    <>
+      {/* 由于Footer的位置跟随Home页面高度的变化而变化，在这里放些内容，调整下Footer的位置，避免样式不好看 */}
+      <h1>home</h1>
+      <h1>home</h1>
+      <h1>home</h1>
+      <h1>home</h1>
+      <h1>home</h1>
+
+    </>
+  );
 }
