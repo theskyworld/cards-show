654bbce8235fe11451383291079de868b324328c
theskyworld <2456450687@qq.com>
2024-08-26 22:38:40 +0800

    feat: 创建resourceSchema和resourcePlaylistSchema数据类型,用于后续添加数据

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,5 +1,81 @@
-import { type SchemaTypeDefinition } from 'sanity'
+import { type SchemaTypeDefinition } from "sanity";
 
+
+const resourceSchema = {
+  name: "resource",
+  title: "Resource",
+  type: "document",
+  fields: [
+    {
+      name: "title",
+      title: "Title",
+      type: "string",
+      require,
+      validation: (Rule: any) => Rule.required(),
+    },
+    {
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      options: { source: "title" },
+    },
+    {
+      name: "downloadLink",
+      title: "Download Link",
+      type: "url",
+      validation: (Rule: any) => Rule.required(),
+    },
+    {
+      name: "views",
+      title: "Views",
+      type: "number",
+      initialValue: 0,
+    },
+    {
+      name: "poster",
+      title: "Poster",
+      type: "image",
+      validation: (Rule: any) => Rule.required(),
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: "category",
+      title: "Category",
+      type: "string",
+      validation: (Rule: any) => Rule.required(),
+      options: {
+        list: ["frontend", "backend", "next 13", "fullstack", "other"],
+      },
+    },
+  ],
+};
+
+const resourcePlaylistSchema = {
+  name: "resourcePlaylist",
+  title: "Resource Playlist",
+  type: "document",
+  fields: [
+    {
+      name: "title",
+      title: "Title",
+      type: "string",
+      validation: (Rule: any) => Rule.required(),
+    },
+    {
+      name: "resources",
+      title: "Resources",
+      type: "array",
+      of: [
+        {
+          type: "reference",
+          to: [{ type: "resource" }],
+        },
+      ],
+    },
+  ],
+};
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
-}
+  types: [resourceSchema,resourcePlaylistSchema],
+};
